75f0ea06e840e2ce1b327f72d44e1ac92fcc3574
yeeyama <samsung36954@gmail.com>
2017-11-19 14:50:30 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110078/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110078.rmutsv.com;
+
+public class HelloWorld {
+//    create main_method
+public static void main(String[] args) {
+
+  System.out.println("Hello World' I'am Day");
+    
+
+
+
+}//main
+
+
+}//class
