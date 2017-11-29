166de2b550637fffbd538484e3a9bcdeaffc93fe
yeeyama <samsung36954@gmail.com>
2017-11-29 09:38:00 +0700

    Testing Condition

---
+++ src/oop_lab3/TestCondition.java
@@ -0,0 +1,24 @@
+package oop_lab3;
+
+public class TestCondition {
+    public static void main(String[] args) {
+        //     if
+        int x = 10, y = 20;
+        if (x < y)
+            System.out.println("x less than y");
+        System.out.println("OOP");
+
+        //      if-else
+        if (x < y)
+            System.out.println("x less than y");
+        else
+            System.out.println("x more than y");
+        //    if-else-if
+        if (x < 30) System.out.println("x less than 30");
+        else if (x < 30) System.out.println("x more than 30");
+        else System.out.println("x equal 30");
+
+
+    }//main
+
+}//class
