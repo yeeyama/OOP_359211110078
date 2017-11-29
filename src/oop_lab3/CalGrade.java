b69998716ce3e08fcba5f392cb5e48696aa7e1cf
yeeyama <samsung36954@gmail.com>
2017-11-29 10:26:26 +0700

    Testing Grade

---
+++ src/oop_lab3/CalGrade.java
@@ -0,0 +1,26 @@
+//condition
+//        score 0-49 grade F
+//        score 50-59 grade D
+//        score 60-69 grade C
+//        score 70-79 grade B
+//        score 80-100 grade A
+package oop_lab3;
+
+import java.util.Scanner;
+
+public class CalGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score;
+        System.out.println("Enter you score: ");
+        score = scanner.nextInt();
+ //         test condition score
+        if (score < 49) System.out.println("You grade is F");
+        else if (score < 59) System.out.println("You grade is D");
+        else if (score < 69) System.out.println("you grade is C");
+        else if (score < 79) System.out.println("You grade is B");
+        else System.out.println("You grade is A");
+
+
+    }// main
+}// class
---
+++ src/oop_lab3/TestSwtich.java
@@ -0,0 +1,19 @@
+package oop_lab3;
+
+import java.util.Scanner;
+
+public class TestSwtich {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // switch-case
+        int select;
+        System.out.print("Please enter name 1-3: ");
+        select = scanner.nextInt();
+        switch (select){
+            case 1: System.out.println("Your entered 1");break;
+            case 2: System.out.println("Your entered 2");break;
+            case 3: System.out.println("Your entered 3");break;
+        }//switch
+
+    }// main
+}// class
