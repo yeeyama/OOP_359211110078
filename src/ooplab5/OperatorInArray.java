d58384c8878b12d8d4fe939c3de6b845499f8d66
yeeyama <samsung36954@gmail.com>
2017-12-13 10:08:54 +0700

    Testing DatatoArray

---
+++ src/ooplab5/OperatorInArray.java
@@ -0,0 +1,24 @@
+package ooplab5;
+
+public class OperatorInArray {
+    public static void main(String[] args) {
+        int number[] = {1, 2, 3, 4, 5};
+        String msg[] = {"Cat", "Dog", "Bird"};
+        System.out.println(msg.length);
+        int number2[] = {1, 2, 3};
+        number2 =number;
+        for (int i=0;i<number2.length;i++)
+            System.out.print(number2[i]+" ");
+
+        int number3[] = {1, 2, 3, 4, 5};
+
+
+
+
+    }//main
+}//class
+
+
+
+
+
--- src/ooplab5/TestDataToArray.java
+++ src/ooplab5/TestDataToArray.java
@@ -1,4 +1,57 @@
 package ooplab5;
+//1. user input data to array
+//2. show data in array
+//3. summation data in array
+//4. average value in array
+
+import java.util.Scanner;
 
 public class TestDataToArray {
+    public static final int  MAX = 5;
+    public static int number[] = new int[MAX];
+    public static void main(String[] args) {
+
+        inputData(number,number.length);
+        showData(number,number.length);
+        summationData(number,number.length)
+
+    }//main
+
+    private static void summationData(int[] number, int length) {
+        double total = 0.0;
+        System.out.print("Total data in array: ");
+        for (int i = 0; i < length; i++){
+            total += number[i];
+        }
+        System.out.print(total+"\a");
+        System.out.println("The avareage data in array: "+
+            (total/length));
+
+    }//summationData
+
+
+    private static void showData(int[] number, int length) {
+        System.out.println("Data in array: ");
+        for (int i=0;i<length;i++){
+            System.out.println(number[i]+" ");
+        }
+        System.out.println();
+    //showData
+
+    private static void inputData(int[] number, int length) {
+        //user input data
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0;i<length;i++)
+    }
+    System.out.print("Enter an integer["+i+"]: ");
+    number[i] = scanner.nextInt();
 }
+
+
+
+
+    }//inputData
+
+
+
+}//class
