f70b9b34dec0eb2dffe9698a9db6850196d3b5c7
yeeyama <samsung36954@gmail.com>
2017-12-13 11:05:33 +0700

    Array2D

--- src/ooplab5/Array2D.java
+++ src/ooplab5/Array2D.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Array2D {
     public static void main(String[] args) {
-//        array2D
+//        array 2D
         Scanner scanner = new Scanner(System.in);
         int number[][] = new int[2][3];
         for (int i=0;i<number.length;i++){
@@ -15,17 +15,17 @@ public class Array2D {
         }//for i
         showArray(number);
 
-
     }//main
 
-    public static void showArray (int[][] number) {
+    private static void showArray(int[][] number) {
         System.out.println("Data in Array: ");
         for (int i=0;i<number.length;i++){
             for (int j=0;j<number[i].length;j++){
-                System.out.print(number[i][j]+" "]);
+                System.out.print(number[i][j]+" ");
             }//for j
             System.out.println();
         }//for i
+
 
     }//showArray
 
--- src/ooplab5/TestArrayList.java
+++ src/ooplab5/TestArrayList.java
@@ -1,7 +1,24 @@
 package ooplab5;
 
+import java.util.ArrayList;
+
 public interface TestArrayList {
     public static void main(String[] args) {
+    //ArrayList
+        ArrayList list = new ArrayList();
+        list.add("SAM");
+        list.add("DAY");
+        list.add("MIT");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(1);
+        System.out.println(list);
+        list.add(1,"YaYa");
+        System.out.println(list);
+        //  print with for loop
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }//for
 
 
 
